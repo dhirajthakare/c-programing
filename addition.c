b66cddc0727a7d8c4ccdad4ca6b48bdf749a2bd3
dhirajthakare <32159491+dhirajthakare@users.noreply.github.com>
2019-08-05 21:05:06 +0530

    c program opf addition of two number

---
+++ addition.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a,b,sum;
+printf("Enter the two number");
+scanf("%d%d",&a,&b);
+sum=a+b;
+printf("\n addition of two number :");
+printf("%d",sum);
+return 0;
+}
