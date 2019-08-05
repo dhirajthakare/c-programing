9ba046b62b277efeccda8ce46f07593aa66e3ab5
dhirajthakare <32159491+dhirajthakare@users.noreply.github.com>
2019-08-05 20:47:33 +0530

    additon of two number

---
+++ addition.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b.sum;
+cout<<"Enter the two number";
+cin>>a>>b;
+sum=a+b;
+cout<<"/n addition of two number :";
+cout<<c;
+
+return 0;
+}
